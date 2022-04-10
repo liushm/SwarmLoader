2f33b25aa080c3ae1d03db5fc06ca4d9cdd87e5a
liushm <liushm.cn@gmail.com>
2022-04-10 13:02:44 +0800

    initial commit

---
+++ swarmloader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Swarm
+{
+    public class Agent
+    {
+        public static int Initialize(string unused)
+        {
+            // AppDomain.CurrentDomain.AssemblyResolve += Current_Resolver;
+
+            AppDomain.CurrentDomain.SetData("APPBASE", Directory.GetCurrentDirectory());
+
+            return 0;
+        }
+
+        static Assembly Current_Resolver(object sender, ResolveEventArgs args)
+        {
+            if (args.Name.Contains("AInterface"))
+            {
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "AInterface.dll");
+
+                return Assembly.LoadFrom(path);
+            }
+
+            return null;
+        }
+    }
+}
